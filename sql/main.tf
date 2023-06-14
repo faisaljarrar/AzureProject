3468d9847a90c9d0ceb22e21119663fd108d9642
ARakhmanov00 <Abdulazizrakhmanov@gmail.com>
2023-06-14 19:02:11 +0000

    v

--- sql/main.tf
+++ sql/main.tf
@@ -17,8 +17,7 @@ resource "azurerm_sql_server" "sql" {
   name                         = var.sql_server_name
   resource_group_name          = azurerm_resource_group.rg.name
   location                     = azurerm_resource_group.rg.location
-  
+  version                      = "12.0"
   administrator_login          = "mysqladminun"
   administrator_login_password = "H@Sh1CoR3!"
-  version    = "5.7"
 }
