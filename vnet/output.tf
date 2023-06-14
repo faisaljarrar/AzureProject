6c476742422bd9b9e7cb0a98e93cf21578309347
ARakhmanov00 <Abdulazizrakhmanov@gmail.com>
2023-06-14 18:30:04 +0000

    done

--- vnet/output.tf
+++ vnet/output.tf
@@ -1,34 +1,16 @@
-output "vnet_address_space" {
-  description = "The address space of the newly created vNet"
-  value       = azurerm_virtual_network.vnet.address_space
-}
-
-output "vnet_guid" {
-  description = "The GUID of the newly created vNet"
-  value       = azurerm_virtual_network.vnet.guid
-}
-
+# Output the VNet and subnet details
 output "vnet_id" {
-  description = "The id of the newly created vNet"
-  value       = azurerm_virtual_network.vnet.id
+  value = azurerm_virtual_network.example.id
 }
 
-output "vnet_location" {
-  description = "The location of the newly created vNet"
-  value       = azurerm_virtual_network.vnet.location
+output "public1_subnet_id" {
+  value = azurerm_subnet.public1.id
 }
 
-output "vnet_name" {
-  description = "The Name of the newly created vNet"
-  value       = azurerm_virtual_network.vnet.name
+output "public2_subnet_id" {
+  value = azurerm_subnet.public2.id
 }
 
-output "vnet_subnets" {
-  description = "The ids of subnets created inside the newly created vNet"
-  value       = local.azurerm_subnets[*].id
-}
-
-output "vnet_subnets_name_id" {
-  description = "Can be queried subnet-id by subnet name by using lookup(module.vnet.vnet_subnets_name_id, subnet1)"
-  value       = local.azurerm_subnets_name_id_map
+output "public3_subnet_id" {
+  value = azurerm_subnet.public3.id
 }
--- vnet/provider.tf
+++ vnet/provider.tf
@@ -1,3 +1,8 @@
+# Configure Azure provider
 provider "azurerm" {
+  subscription_id = var.azure_subscription_id
+  client_id       = var.azure_client_id
+  client_secret   = var.azure_client_secret
+  tenant_id       = var.azure_tenant_id
   features {}
 }
