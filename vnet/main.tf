458402c40e16b041cfd46e48c7e8a826b132e674
ARakhmanov00 <Abdulazizrakhmanov@gmail.com>
2023-06-14 19:40:48 +0000

    d

--- vnet/main.tf
+++ vnet/main.tf
@@ -3,9 +3,7 @@ terraform {
     resource_group_name  = "StorageAccount-ResourceGroup"
     storage_account_name = "team2project"
     container_name       = "tfstate"
-    key                  = "path/to/my/key/prod.terraform.tfstate"
-    access_key = "pbdzjjYmnpXTUmYIi/bLxl5qhq+iDbkHXCTFe+UhTwi1UoF1ZvzOszr/KcZFXtkvLPgm+YiyX6NI+AStIDDJsA=="
-  }
+    }
 }
 
 
