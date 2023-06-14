aab61ce97602b203c2e88da3823b71957eb6e9b0
ARakhmanov00 <Abdulazizrakhmanov@gmail.com>
2023-06-14 18:33:26 +0000

    done

--- vnet/variables.tf
+++ vnet/variables.tf
@@ -1,20 +1,4 @@
 # Define variables
-variable "azure_subscription_id" {
-  description = "Azure Subscription ID"
-}
-
-variable "azure_client_id" {
-  description = "Azure Client ID"
-}
-
-variable "azure_client_secret" {
-  description = "Azure Client Secret"
-}
-
-variable "azure_tenant_id" {
-  description = "Azure Tenant ID"
-}
-
 variable "azure_location" {
   description = "Azure Region"
   default     = "West Europe"
