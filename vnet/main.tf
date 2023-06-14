9205c184f4216983f653fb42f00491cfa27fab5d
ARakhmanov00 <Abdulazizrakhmanov@gmail.com>
2023-06-14 19:49:15 +0000

    l

--- vnet/main.tf
+++ vnet/main.tf
@@ -1,10 +1,27 @@
+resource "azurerm_resource_group" "terraform" {
+  name     = "StorageAccount-ResourceGroup"
+  location = "westus"
+}
+
+resource "azurerm_storage_account" "terraform" {
+  name                     = "team2project"
+  resource_group_name      = azurerm_resource_group.terraform.name
+  location                 = azurerm_resource_group.terraform.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "Production"
+  }
+}
+
 terraform {
   backend "azurerm" {
     resource_group_name  = "StorageAccount-ResourceGroup"
     storage_account_name = "team2project"
     container_name       = "tfstate"
     key                  = "path/to/my/key/prod.terraform.tfstate"
-    access_key = "pbdzjjYmnpXTUmYIi/bLxl5qhq+iDbkHXCTFe+UhTwi1UoF1ZvzOszr/KcZFXtkvLPgm+YiyX6NI+AStIDDJsA=="
+    access_key = azurerm_storage_account.terraform.primary_access_key
   }
 }
 
